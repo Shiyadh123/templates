02ed1bc6c5c0e799f240a9d72e110b591110a406
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-07-15 09:57:51 +0530

    Update dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -9,25 +9,30 @@ public:
         return true;
     }
     int dfs(int i,int j,vector<vector<int>>& grid,vector<vector<bool>>& vis){
-        int area=0;
         if(!isValid(i,j,grid,vis)) return area;
-        vis[i][j]=1;
-        area=1;
+        
+        vis[i][j]=1;  // OR set grid[i][j]=0;
+        int area=1;
+        
         for(auto dir:dirs){
             area+=dfs(i+dir[0],j+dir[1],grid,vis);
         }
+        
         return area;
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<bool>> vis(m,vector<bool>(n,0));
+        
+        vector<vector<bool>> vis(m,vector<bool>(n,0)); // OR set grid[i][j]=0;
         int maxi=0;
+        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                     maxi=max(maxi,dfs(i,j,grid,vis));
             }
         }
+        
         return maxi;
     }
 };
